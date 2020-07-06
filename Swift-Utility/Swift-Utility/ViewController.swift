0a2c31951e9442ecfce92d356c726cb156692643
Pham Quang Huy <pqhuy87it@gmail.com>
2020-07-07 01:21:40 +0900

    update.

---
+++ Swift-Utility/Swift-Utility/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Swift-Utility
+//
+//  Created by Pham Quang Huy on 2020/05/21.
+//  Copyright © 2020 Pham Quang Huy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+		randomStringUsage()
+    }
+
+    func delayUsage() {
+        delay(bySeconds: 1.5) {
+            // delayed code
+        }
+        
+        delay(bySeconds: 1.5, dispatchLevel: .background) {
+            // delayed code that will run on background thread
+        }
+    }
+
+	func nsRegualrExpressionUsage() {
+		let string = "🇩🇪€4€9"
+		let matched = matches(for: "[0-9]", in: string)
+		print(matched)
+	}
+
+	func randomStringUsage() {
+		print(randomString(length: 20))
+	}
+
+}
+
